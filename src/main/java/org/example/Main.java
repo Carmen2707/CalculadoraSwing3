c6bfce197542a3e5e6a35d88a59ee9e3d64a360f
Carmen <carmengarciagr27@gmail.com>
2023-10-20 22:19:22 +0200

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,9 @@ import ui.VentanaCalculadora;
 
 
 public class Main {
+    /**
+     * @param args Este método main crea una ventana y utiliza el método load para mostrarla y ejecutarla.
+     */
     public static void main(String[] args) {
         VentanaCalculadora ventanaCalculadora = new VentanaCalculadora();
         ventanaCalculadora.load();
