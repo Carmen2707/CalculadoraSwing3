671b35348599ae1a69ac4e942fa03b454cc2c69b
Carmen <carmengarciagr27@gmail.com>
2023-10-20 21:49:23 +0200

    Initial commit

---
+++ src/main/java/ui/VentanaCalculadora.java
@@ -0,0 +1,33 @@
+package org.example.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class VentanaCalculadora extends JFrame {
+    public VentanaCalculadora() throws HeadlessException {
+    }
+
+    private JPanel resultado;
+    private JButton a1Button;
+    private JButton a8Button;
+    private JButton a3Button;
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton a2Button;
+    private JButton a7Button;
+    private JButton a9Button;
+    private JButton a0Button;
+    private JButton btnsumar;
+    private JButton btnrestar;
+    private JButton btnmultiplicar;
+    private JButton btndividir;
+    private JButton btnborrar;
+    private JButton btnraiz;
+    private JButton btnigual;
+    private JButton btncomaButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
